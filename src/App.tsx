e12d14c1c10d7e3d44fed14bef8d400556e8d6e5
mansur <mansur_dzhamaldinov.@mail.ru>
2023-11-21 17:59:58 +0300

    готово

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-
-
 import { Books } from "./components/Books/index";
-
 import Header from "./components/Header";
 import styles from "./header.module.css";
-
 import FullPage from "./components/FullPage/Index"
 
 function App() {
   return (
     <>
-
-<Books/>
-
-     <Header />
+    <Books/>
+    <Header />
     <FullPage/>
-
     </>
   );
 }
