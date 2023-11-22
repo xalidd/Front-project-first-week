c3450eadf74bbafef35c3281223593a704114575
Khalid <xalidd999@mail.ru>
2023-11-22 16:50:39 +0300

    сделал полностью карточку

--- src/components/Books/index.tsx
+++ src/components/Books/index.tsx
@@ -3,6 +3,8 @@ import book1 from "../../assets/book1.jpg";
 import book2 from "../../assets/book2.jpg";
 import book3 from "../../assets/book3.jpg";
 import book4 from "../../assets/book4.jpg";
+import basket from "../../assets/basket.png";
+
 import style from "./bookShop.module.css";
 
 export const Books = () => {
@@ -12,23 +14,24 @@ export const Books = () => {
         <img src={book1} alt="" sizes="" srcset="" />
         <div className={style.line_1}>
           <div className="left">
-            <span className={style.oldPrice}>1450 руб</span>
-            <span className={style.actuaclPrice}>990 руб</span>
+            <span className={style.oldPrice}>1450 ₽</span>
+            <span className={style.actuaclPrice}>990 ₽</span>
           </div>
           <div className={style.right}>
-            <span>9%</span>
+            <span className={style.sale}>20%</span>
           </div>
         </div>
 
         <div className={style.line_2}>
-          <div className="title_head">Нигель</div>
-          <div className="title_author">Саша Карин</div>
+          <div className={style.title_head}>Нигель</div>
+          <div className={style.title_author}>Саша Карин</div>
         </div>
 
         <div className={style.line_3}>
-        <button className={style.btn_bye}>Купить</button>
-        <button>save</button>
-
+          <button className={style.btn_bye}>Купить</button>
+          <button className={style.savaBook}>
+            <img src={basket} alt="" width={24} height={24} />
+          </button>
         </div>
       </div>
     </div>
