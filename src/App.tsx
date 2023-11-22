cd6e3f47710da652ffc116285757a41696aa8439
mansur <mansur_dzhamaldinov.@mail.ru>
2023-11-22 11:06:22 +0300

    готово

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { Books } from "./components/Books/index";
 import Header from "./components/Header";
 import styles from "./header.module.css";
 import FullPage from "./components/FullPage/Index"
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-    <Books/>
-    <Header />
-    <FullPage/>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/books" element={<FullPage/>}/>
+          сверху роут на 16 стр должен быть роут по айди нужно дополнить т.е. /books/booksId
+        </Routes>
+      </div>
+    </div>
     </>
   );
 }
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Books } from "../Books";
+
+const Home = () => {
+    return (
+        <>
+          <Books/>  
+        </>
+    );
+};
+
+export default Home;
