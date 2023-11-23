8d665294884677e332f858578a81f55f12804af6
mansur <mansur_dzhamaldinov.@mail.ru>
2023-11-23 21:36:03 +0300

    ready

--- src/components/Books/index.tsx
+++ src/components/Books/index.tsx
@@ -28,37 +28,42 @@ export const Books = () => {
   );
 
   const fromInput = useSelector((state) => state.books.inputValue);
-
+    console.log(fromInput);
+    
   const perPageBooks = useMemo(() => {
     return books.slice(currentPage * perPage - perPage, currentPage * perPage);
   }, [currentPage, books]);
 
   const choosedBook = perPageBooks.filter((item) => {
-    if (item.name.toLowerCase().includes(fromInput)) {
+    if (item.name.toLowerCase().includes(fromInput.toLowerCase())) {
       return true;
     }
     return false;
   });
   return (
     <>
-
-    <div className={style.book}>
-      {choosedBook.map((item, i) => (
-        <div className={style.book_nom1}>
-          <Link to={`/books/${item._id}`}><img src={item.image} alt="" sizes="" srcset="" /></Link>
-          <div className={style.line_1}>
-            <div className="left">
-              <span className={style.oldPrice}>1450 ₽</span>
-              <span className={style.actuaclPrice}>{item.price}</span>
+      <div className={style.book}>
+        {choosedBook.map((item, i) => (
+          <div className={style.book_nom1}>
+            <Link to={`/books/${item._id}`}>
+              <img src={item.image} alt="" sizes="" srcset="" />
+            </Link>
+            <div className={style.line_1}>
+              <div className="left">
+                <span className={style.oldPrice}>1450 ₽</span>
+                <span className={style.actuaclPrice}>{item.price}</span>
+              </div>
+              <div className={style.right}>
+                <span className={style.sale}>20%</span>
+              </div>
             </div>
-            <div className={style.right}>
-              <span className={style.sale}>20%</span>
-            </div>
 
-          <div className={style.line_2}>
-            <div className={style.title_head}><Link to={`/books/${item._id}`}>{item.name}</Link></div>
-            <div className={style.title_author}>{item.author}</div>
-          </div>
+            <div className={style.line_2}>
+              <div className={style.title_head}>
+                <Link to={`/books/${item._id}`}>{item.name}</Link>
+              </div>
+              <div className={style.title_author}>{item.author}</div>
+            </div>
 
             <div className={style.line_3}>
               <button className={style.btn_bye}>Купить</button>
